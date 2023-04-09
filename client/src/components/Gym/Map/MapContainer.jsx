456622178dca592850ba8d085a831627aa31ba5b
kimyonghui <kyhui1115@gmail.com>
2023-04-10 02:43:13 +0900

    comment: 헬스장 상세 지도 주석 추가

--- client/src/components/Gym/Map/MapContainer.jsx
+++ client/src/components/Gym/Map/MapContainer.jsx
@@ -6,13 +6,14 @@ function GymMapContainer({ gymAddress }) {
   const [map, setMap] = useState({ Ma: 0, La: 0 });
 
   useEffect(() => {
-    // 주소-좌표 변환 객체를 생성합니다
+    // 주소-좌표 변환 객체를 생성
     const geocoder = new kakao.maps.services.Geocoder();
 
-    // 주소로 좌표를 검색합니다
+    // 주소로 좌표를 검색
     geocoder.addressSearch(gymAddress, (result, status) => {
       // 정상적으로 검색이 완료됐으면
       if (status === kakao.maps.services.Status.OK) {
+        // 위도, 경도를 가져온다.
         const coords = new kakao.maps.LatLng(result[0].y, result[0].x);
         setMap(coords);
       }
@@ -21,6 +22,7 @@ function GymMapContainer({ gymAddress }) {
 
   return (
     <Map center={{ lat: map.Ma, lng: map.La }} className="w-full h-64 mt-1">
+      {/* 주소를 지도에 표시 */}
       <MapMarker position={{ lat: map.Ma, lng: map.La }} />
     </Map>
   );
